1ea7cf6567c21ffa51b7d043ad13903063477598
Robertson Solani Vicente <rsolani@gmail.com>
2019-03-12 02:24:31 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AgileContent
+{
+    static class Siblings
+    {
+        private static void Main(string[] args)
+        {
+            GetLargestSibling(123);
+        }
+
+        static void GetLargestSibling(int number)
+        {   
+            var digits = new List<int>();
+            
+            for (;number != 0; number /= 10)
+                digits.Add(number % 10);
+            
+            for (var num = 0; num < digits.Count; num++)
+            {
+                for (var sort = 0; sort < digits.Count - 1; sort++)
+                {
+                    if (digits[sort] >= digits[sort + 1])
+                        continue;
+
+                    var temp = digits[sort + 1];
+                    digits[sort + 1] = digits[sort];
+                    digits[sort] = temp;
+                }
+            }
+
+            foreach (var t in digits)
+                Console.Write(t + "");
+
+            Console.ReadKey();
+        }
+    }
+}
